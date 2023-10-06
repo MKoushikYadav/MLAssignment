7d2cd8535ace886760b240a9dc91110898927e59
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2023-10-06 20:23:23 +0530

    Added another unit test

--- test.py
+++ test.py
@@ -5,14 +5,29 @@ from app import app
 
 class PredictionTest(unittest.TestCase):
 
-    def test_predict(self):
-        """Tests that the predict function makes the correct prediction for a given input."""
+    def test_predict1(self):
+        """Tests that the predict function returns a value equal to the input if less than threshold (441) """
 
         input_data = {
-            "number":100
+            "number":200
         }
 
-        expected_prediction = 100
+        expected_prediction = 200
+
+        response = app.test_client().post("/", json=input_data)
+        print(response)
+        actual_prediction = json.loads(response.data)["prediction"]
+
+        self.assertEqual(actual_prediction, expected_prediction)
+        
+    def test_predict2(self):
+        """Tests that the predict function makes the correct predictions for values greater than threshold (441)"""
+
+        input_data = {
+            "number":1000
+        }
+
+        expected_prediction = 589
 
         response = app.test_client().post("/", json=input_data)
         print(response)
